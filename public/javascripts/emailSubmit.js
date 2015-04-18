d15fe856d8275138f96e2336eab66aeb09b62dbf
Fernando Trujano <trujano@mit.edu>
2015-04-18 19:58:05 -0400

    Implemented email sending based on last day Jobs are automatically scheduled based on time left till date specified.

--- public/javascripts/emailSubmit.js
+++ public/javascripts/emailSubmit.js
@@ -6,8 +6,9 @@ $(document).ready(function(){
             'subject': $("#subject").val(),
             'text' : $("#text").val(),
             'html' : $("#html").val(),
-            'usersConfirmed' : ['user1', 'user2'],
-            'id' : Math.random().toString(36).substring(7)
+            'id' : Math.random().toString(36).substring(7),
+            'date' : new Date($("#date").val())
+            //usersConfirmed added on server
         };
 
 
--- routes/email.js
+++ routes/email.js
@@ -16,56 +16,64 @@ router.get('/', function(req, res, next) {
 * Schedule jobs several jobs until deadline
 */
 router.post('/new', function(req, res, next) {
-    console.log("Hi therea")
+    console.log("Hi therea");
    // console.log(req.body)
 
     //Add email to db
     var newEmail = req.body;
-    newEmail['usersConfirmed'] = []
+    newEmail['usersConfirmed'] = [];
+    newEmail['lastDay'] = new Date(newEmail.date);
 
-    console.log(newEmail)
+    console.log(newEmail);
+
     var db = req.db;
-    var emails = db.collection('emails')
+    var emails = db.collection('emails');
     emails.insert(newEmail, function(err,doc){
-        if (err) console.log("There was an error with the db")
-        else console.log("Email added to db")
+        if (err) console.log("There was an error with the db");
+        else console.log("Email added to db");
 
-
-        var schedule = require('node-schedule')
-        var date = new Date();
-        sendEmailtoUnconfirmed(newEmail, db);
-
-        date.setSeconds(date.getSeconds() + 30)
-        var j = schedule.scheduleJob(date,function(){
-            sendEmailtoUnconfirmed(newEmail, db);
-        })
-
-
-    })
-
-    //Schedule Jobs
-
-
-
-
-
-
-    // console.log("Scheduling...")
-    // var schedule = require('node-schedule')
-
-    // for (var i = 1; i<3; i++){
-    //     var date = new Date()
-    //     date.setSeconds(date.getSeconds() + (i*10));
-
-    //     var j = schedule.scheduleJob(date, function(){
-    //         console.log("HAIIII")
-    //     })
-    // }
-
-    //res.send("Got it!")
-
+        scheduleEmail(newEmail, db);
+    });
 
 });
+
+/*
+* Sends an email and schedules the next one
+*/
+function scheduleEmail(email, db){
+    sendEmailtoUnconfirmed(email, db);
+
+    console.log("Scheduling email");
+
+    //Schedule next email
+    var schedule = require('node-schedule');
+
+    var today = new Date(); //Today's date
+    var nextDay = new Date(); //date for next reminder
+    var lastDay = email.lastDay;
+
+    var daysLeft = Math.round((lastDay - today)/(1000*60*60*24));
+
+    console.log("Days Left: " + daysLeft);
+
+    if (daysLeft < 0 ) return ;//Email is done
+    //How often to send emails depending on daysLeft
+    else if (daysLeft > 30) nextDay.setDays(today.getDays() + 7);
+    else if (daysLeft > 14) nextDay.setDays(today.getDays() + 3);
+    else if (daysLeft > 7) nextDay.setDays(today.getDays() + 1);
+    else if (daysLeft > 5) nextDay.setDays(today.getDays() + 0.5);
+    else nextDay.setHours(today.getHours() + 1);
+
+    //nextDay.setSeconds(today.getSeconds() + 30); //send every 30 seconds
+
+    //Schedule an email for the next day
+    var j = schedule.scheduleJob(nextDay,function(){
+        scheduleEmail(email,db);
+    });
+    console.log("Next email scheduled for:" + nextDay.toDateString());
+
+
+}
 
 /*
 * Sends email to all users that haven't confirmed
@@ -74,23 +82,23 @@ router.post('/new', function(req, res, next) {
 */
 
 function sendEmailtoUnconfirmed(email, db){
-    console.log("Sending email")
+    console.log("Sending email");
     //Get email from the db
     db.collection('emails').findOne({id: email.id}, function(err, result){
         if (!err && result){
-            console.log(result)
+            //console.log(result);
             var usersConfirmed = result.usersConfirmed;
-            console.log("Users Confirmed: ")
-            console.log(usersConfirmed)
+            console.log("Users Confirmed: ");
+            console.log(usersConfirmed);
             //Get all users
             db.collection('userlist').find().toArray(function (err, users) {
                 if (!err && users){
-                    for (i in users){
-                        console.log(users[i].name)
+                    for (var i in users){
+                        //console.log(users[i].name);
                         if (usersConfirmed.indexOf(users[i].id) < 0){ //user is not confirmed
-                            sendEmail(email, users[i])
+                            sendEmail(email, users[i]);
                         }
-                        else console.log(user[i].name + " already confirmed!")
+                        else console.log(users[i].name + " already confirmed!");
                     }
 
                 }
@@ -98,15 +106,46 @@ function sendEmailtoUnconfirmed(email, db){
 
         }
 
-    })
+    });
 
 
 }
 
 // Sends the email to the reciever
 function sendEmail(email, reciever){
-    console.log("Sending Email to " + reciever.name)
-    //TODO
+    console.log("Sending Email to " + reciever.name);
+    var confirmURL = 'http://localhost:3000/users/confirmEmail/'+reciever.id+'/'+email.id;
+
+    var nodemailer = require('nodemailer');
+
+    // create reusable transporter object using SMTP transport
+    var transporter = nodemailer.createTransport({
+        service: 'Gmail',
+        auth: {
+            user: 'sentimentalprinceton@gmail.com',
+            pass: 'hack123hack'
+        }
+    });
+
+    // setup e-mail data with unicode symbols
+    var mailOptions = {
+        from: 'Email Annoyer ✔ <email@annoyer.com>', // sender address
+        to: reciever.email, // list of receivers
+        subject: email.subject, // Subject line
+        text: email.text.replace(/##NAME##/g, reciever.name).replace(/##CONFIRM##/g, confirmURL), // plaintext body
+        html: email.html.replace(/##NAME##/g, reciever.name).replace(/##CONFIRM##/g, confirmURL) // html body
+    };
+
+    // send mail with defined transport object
+    transporter.sendMail(mailOptions, function(error, info){
+        if(error){
+            console.log(error);
+        }else{
+            console.log('Message sent: ' + info.response);
+        }
+    });
+
+
 }
 
 module.exports = router;
